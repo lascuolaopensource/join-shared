249a6bc9467a248233c593625e2c02f7f2f1c925
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-20 09:39:56 +0200

    Update update-slots.ts

--- src/endpoints/admin-tools/update-slots.ts
+++ src/endpoints/admin-tools/update-slots.ts
@@ -1,11 +1,3 @@
-import { Enum_Toolslot_Type } from "../../types";
+import { ToolSlotInput } from "../../types";
 
-export type SlotUpdate = {
-	toolID: string;
-	dateTime: string;
-	state: Enum_Toolslot_Type | null;
-};
-
-export type AdminToolsUpdateSlotsReq = {
-	changes: Array<SlotUpdate>;
-};
+export type AdminToolsUpdateSlotsReq = Record<string, Array<ToolSlotInput>>;
